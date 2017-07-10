8ddbc2bcbf8311beb633c2bdeb0959e2d05231cd
Nikolay Aksenov <inikolay.axenov@gmail.com>
2017-07-10 15:32:24 +0300

    Initial Commit

---
+++ NanoTabBar/NanoTabBar.h
@@ -0,0 +1,19 @@
+//
+//  NanoTabBar.h
+//  NanoTabBar
+//
+//  Created by Nikolay Aksenov on 10.07.17.
+//  Copyright © 2017 Nikolay Aksenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NanoTabBar.
+FOUNDATION_EXPORT double NanoTabBarVersionNumber;
+
+//! Project version string for NanoTabBar.
+FOUNDATION_EXPORT const unsigned char NanoTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NanoTabBar/PublicHeader.h>
+
+
